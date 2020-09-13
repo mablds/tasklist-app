b67cbad318eb288876fd76eae4701a1a80783605
mablds <marcelobragalemos@gmail.com>
2020-09-13 02:28:39 -0300

    update log file names

--- helpers/logger.js
+++ helpers/logger.js
@@ -4,27 +4,27 @@ module.exports.logger =  winston.createLogger({
   transports: [
     new winston.transports.File({
       level: 'info',
-      filename: './logs/filelog-info.log',
+      filename: './logs/tivit-task-list.log',
       json: true,
-      format: winston.format.combine(winston.format.timestamp(), winston.format.json())
+      format: winston.format.combine(winston.format.colorize(), winston.format.timestamp(), winston.format.json())
     }),
     new winston.transports.File({
       level: 'error',
-      filename: './logs/filelog-error.log',
+      filename: './logs/tivit-task-list.log',
       json: true,
-      format: winston.format.combine(winston.format.timestamp(), winston.format.json())
+      format: winston.format.combine(winston.format.colorize(), winston.format.timestamp(), winston.format.json())
     }),
     new winston.transports.File({
       level: 'warn',
-      filename: './logs/filelog-warn.log',
+      filename: './logs/tivit-task-list.log',
       json: true,
-      format: winston.format.combine(winston.format.timestamp(), winston.format.json())
+      format: winston.format.combine(winston.format.colorize(), winston.format.timestamp(), winston.format.json())
     }),
     new winston.transports.File({
       level: 'debug',
-      filename: './logs/filelog-debug.log',
+      filename: './logs/tivit-task-list-debug.log',
       json: true,
-      format: winston.format.combine(winston.format.timestamp(), winston.format.json())
+      format: winston.format.combine(winston.format.colorize(), winston.format.timestamp(), winston.format.json())
     })
   ]
 });
