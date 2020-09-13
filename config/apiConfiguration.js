c8c8bb88bcd2dbc8f2a9473052a69db407283b64
mablds <marcelobragalemos@gmail.com>
2020-09-13 03:16:36 -0300

    update log middleware to winston log stream

--- config/apiConfiguration.js
+++ config/apiConfiguration.js
@@ -1,12 +1,18 @@
-require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const morgan = require('morgan')
+const { morganToWinstonStream } = require('../helpers/logger')
 
 module.exports.setUpServer = () => {
     const app = express()
 
+    //Body Parser - JSON
     app.use(express.json())
+    //Enabling Cors
     app.use(cors())
+    //Morgan log middleware setup to Winston's log Stream
+    app.use(morgan("combined", { stream: morganToWinstonStream }))
+    //Routing to Controller Index
     app.use(require('../controllers'))
 
     return app
