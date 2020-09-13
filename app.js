2cb5ed0a6742adfe8e517e56958b6c67fd4cd529
mablds <marcelobragalemos@gmail.com>
2020-09-13 02:13:21 -0300

    logging the api listen function

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
 const config = require('./config/apiConfiguration.js');
+const { logger } = require('./helpers/logger')
 
 const PORT = process.env.PORT || 3000
 const app = config.setUpServer()
 
 app.listen(PORT, () => {
-    console.log(`TIVITaskList rodando na porta ${PORT}!`)
+    logger.debug(`TIVIT Task List server up. Running on Port: ${PORT}!`)
+    console.log(`TIVIT Task List server up. Running on Port: ${PORT}!`)
 })
