495bddcbb646e5965b4ad88f060397ed2a892ea0
mablds <marcelobragalemos@gmail.com>
2020-09-13 03:15:41 -0300

    update variable names

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 require('dotenv').config()
+
 const config = require('./config/apiConfiguration.js');
 const { logger } = require('./helpers/logger')
 
-const PORT = process.env.PORT || 3000
+const port = process.env.PORT || 3000
 const app = config.setUpServer()
 
-app.listen(PORT, () => {
-    logger.debug(`TIVIT Task List server up. Running on Port: ${PORT}!`)
-    console.log(`TIVIT Task List server up. Running on Port: ${PORT}!`)
+app.listen(port, () => {
+    logger.info(`TIVIT Task List server up.`)
+    logger.debug(`TIVIT Task List server up. Running on Port: ${port}!`)
 })
