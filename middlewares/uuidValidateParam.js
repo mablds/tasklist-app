e72a127f6a8837265fe7e50fbc29c9ccfe869f8b
mablds <marcelobragalemos@gmail.com>
2020-09-14 13:55:42 -0300

    update warn logger obj info

--- middlewares/uuidValidateParam.js
+++ middlewares/uuidValidateParam.js
@@ -8,7 +8,7 @@ module.exports = (req, res, next) => {
         logger.debug({ uuid: req.params.id, uuidValid })
         next()
     } else {
-        logger.warn({ uuid: req.params.id, msg: `Invalid UUID. Attention to multiple bad requests.`, })
+        logger.warn({ uuid: req.params.id, msg: `Invalid UUID. Attention to multiple bad requests.`, status: 400 })
         res.status(400).json({
             status: 400,
             msg: `Invalid Parameter. Please, check the Documentation!`
