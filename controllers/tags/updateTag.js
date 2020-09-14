79cdbae6946f5722c2908ba8f783afdc42bfe3eb
mablds <marcelobragalemos@gmail.com>
2020-09-14 18:29:35 -0300

    updateTag endpoint

--- controllers/tags/updateTag.js
+++ controllers/tags/updateTag.js
@@ -1,5 +1,16 @@
+//DAO import
+const updateOneTagDAO = require('../../DAOs/tags/updateOneTag')
 
 //Update Tag Endpoint
-module.exports = (req, res) => {
-    
+module.exports = async (req, res) => {
+//Update One Task list Endpoint
+    if(req.body.name || req.body.active) {
+        const updateTag = await updateOneTagDAO(req.params.id, req.body.name)
+        res.status(updateTag.status).json({ 
+            status: updateTag.status, 
+            msg: updateTag.status == 200 ? 'Tag updated successfully!' : updateTag.msg
+        })
+    } else {
+        res.status(400).json({ status: 400, msg: `Invalid Body! Please check the documentation.` })
+    }
 }
