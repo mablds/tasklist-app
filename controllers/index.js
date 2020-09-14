81cb1f605b9cf82533e4cee94285e12eadd2a0b7
mablds <marcelobragalemos@gmail.com>
2020-09-14 10:50:16 -0300

    update home endpoint response to includ docs

--- controllers/index.js
+++ controllers/index.js
@@ -12,7 +12,10 @@ router.use('/tags', tagsHandler)
 
 // Home Endpoint
 router.get('/', async (req, res) => {
-    res.status(200).json({ msg: `Welcome to TIVIT Task List App - Created by Marcelo Arthur Braga (mablds)!`})
+    res.status(200).json({ 
+        msg: `Welcome to TIVIT Task List App - Created by Marcelo Arthur Braga (mablds)!`,
+        doc: `https://github.com/mablds/tasklist-app`
+    })
 })
 
 module.exports = router
