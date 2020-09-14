b55b89bc672e48dc3427cf8c41671d037bf93b8d
mablds <marcelobragalemos@gmail.com>
2020-09-14 13:29:35 -0300

    get All tags created and logger implemented

---
+++ DAOs/tags/getAllTags.js
@@ -0,0 +1,20 @@
+const database = require('../../models')
+const { logger } = require('../../helpers/logger')
+
+//FindAll TaskLists
+module.exports = async () => {
+    try {
+        const tags = await database.Tags.findAll({
+            where: {
+                active: true
+            }
+        });
+        
+        logger.debug({ status: 200, tags, function: 'DAO - GetAllTags'})
+        return { status: 200, tags }
+        
+    } catch (error) {
+        logger.error({ status: 500, msg: error, function: 'DAO - GetAllTags'})
+        return { status: 500, msg: error }
+    }
+}
