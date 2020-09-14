d204abdea1f425188fbd31d967ff6a0d75b15334
mablds <marcelobragalemos@gmail.com>
2020-09-14 13:34:05 -0300

    logger implemented on the uuidValidate middleware

--- middlewares/uuidValidateParam.js
+++ middlewares/uuidValidateParam.js
@@ -1,11 +1,14 @@
 const uuid = require('uuid')
+const { logger } = require('../helpers/logger')
 
 module.exports = (req, res, next) => {
     let uuidValid = uuid.validate(req.params.id)
     
-    if(uuidValid) { 
+    if(uuidValid) {
+        logger.debug({ uuid: req.params.id, uuidValid })
         next()
     } else {
+        logger.warn({ uuid: req.params.id, msg: `Invalid UUID. Attention to multiple bad requests.`, })
         res.status(400).json({
             status: 400,
             msg: `Invalid Parameter. Please, check the Documentation!`
