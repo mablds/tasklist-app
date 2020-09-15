0c31fec5831ef69135f15460697f34661f506720
mablds <marcelobragalemos@gmail.com>
2020-09-15 04:27:42 -0300

    Create task and TaskTag instance in DB complete

--- DAOs/taskTags/createTaskTag.js
+++ DAOs/taskTags/createTaskTag.js
@@ -3,13 +3,17 @@ const database = require('../../models')
 const { logger } = require('../../helpers/logger')
 
 //Create TaskTag
-module.exports = async (task) => {
+module.exports = async (task, tag) => {
     try {
-        const taskTag = await database.TaskTags.create(task);
+        const taskTag = await database.TaskTags.create({
+            task_id: task,
+            tag_id: tag,
+            active: true
+        });
         logger.debug({ status: 201, msg: 'TaskTag created!', taskTag, function: 'DAO - createTaskTag'})
         return { status: 201, msg: 'TaskTag created!', taskTag }
     } catch (error) {
-        logger.error({ status: 500, error, task , function: 'DAO - createTaskTag'})
-        return { status: 500, msg: error, task }
+        logger.error({ status: 500, msg: error.message, task_id: task, tag_id: tag , function: 'DAO - createTaskTag'})
+        return { status: 500, msg: error.message, task_id: task, tag_id: tag }
     }
 }
--- DAOs/tasks/createTask.js
+++ DAOs/tasks/createTask.js
@@ -2,18 +2,23 @@
 const database = require('../../models')
 const { logger } = require('../../helpers/logger')
 
-const createTag = require('../tags/createOneTag')
-const updateCountTag = require('../tags/updateCountTag')
-
 //Create Task
-module.exports = async (task) => {
+module.exports = async (taskObj) => {
     try {
-        //TODO - Verify obj to create, implement uuid FK reference, Tag creation/count update
-        const task = await database.Task.create(task);
+        const task = await database.Tasks.create({
+            title: taskObj.title,
+            status: taskObj.status,
+            task_list: taskObj.task_list,
+            notes: taskObj.notes ? taskObj.notes : null,
+            priority: taskObj.priority ? taskObj.priority : null,
+            remind_me_on: taskObj.remind_me_on ? taskObj.remind_me_on : null,
+            activity_type: taskObj.activity_type ? taskObj.activity_type : null,
+            active: true
+        });
         logger.debug({ status: 201, msg: 'Task created!', task, function: 'DAO - createTask'})
         return { status: 201, msg: 'Task created!', task }
     } catch (error) {
-        logger.error({ status: 500, error, task , function: 'DAO - createTask'})
-        return { status: 500, msg: error, task }
+        logger.error({ status: 500, error: error.message, taskObj , function: 'DAO - createTask'})
+        return { status: 500, msg: error.message }
     }
 }
