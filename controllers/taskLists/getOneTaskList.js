af813aa689e545038a5034f3c44cc71e5f044108
mablds <marcelobragalemos@gmail.com>
2020-09-16 01:20:55 -0300

    clean getOneTaskList Handler

--- controllers/taskLists/getOneTaskList.js
+++ controllers/taskLists/getOneTaskList.js
@@ -1,14 +1,8 @@
-const uuid = require('uuid')
-
 //DAO import
 const getOneTaskListDAO = require('../../DAOs/taskLists/getOneTaskList')
 
 //Get One Task list
 module.exports = async (req, res) => {
-    if(req.params.id && uuid.validate(req.params.id)) {
-        const findOneTaskList = await getOneTaskListDAO(req.params.id);
-        res.status(findOneTaskList.status).json(findOneTaskList)
-    } else {
-        res.status(400).json({ status: 400, msg: `Invalid Param! Please check the documentation.` })
-    }
+    const findOneTaskList = await getOneTaskListDAO(req.params.id);
+    res.status(findOneTaskList.status).json(findOneTaskList)
 }
